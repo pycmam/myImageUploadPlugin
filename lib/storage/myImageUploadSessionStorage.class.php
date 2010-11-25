87f6ad30525d0e7e2470c4c38953d2302e4aa484
pycmam <pycmam@gmail.com>
2010-11-25 10:19:43 +0500

    fix: общая сессия между субдоменами

--- lib/storage/myImageUploadSessionStorage.class.php
+++ lib/storage/myImageUploadSessionStorage.class.php
@@ -6,14 +6,17 @@ class myImageUploadSessionStorage extends sfSessionStorage
     {
         $context = sfContext::getInstance();
 
-        //if($context->getActionName() == "upload") {
-            $sessionName = $options["session_name"];
+        $sessionName = $options["session_name"];
 
-            if($value = $context->getRequest()->getParameter($sessionName)) {
-                session_name($sessionName);
-                session_id($value);
-            }
-        //}
+        if($value = $context->getRequest()->getParameter($sessionName)) {
+            session_name($sessionName);
+            session_id($value);
+        }
+
+        if (isset($options['session_cookie_domain']) && '.' == $options['session_cookie_domain']) {
+            preg_match('/([^.]+\.[^.]+)$/', $_SERVER['SERVER_NAME'], $matches);
+            $options['session_cookie_domain'] = '.' . $matches[1];
+        }
 
         parent::initialize($options);
     }
